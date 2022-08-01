c4036a8bfcb9bc1dc4252a4e7d88a13cbb1bfd2a
Justin Farnsworth <farnswj1@tcnj.edu>
2022-08-01 10:50:48 -0400

    cleaned up the quotes in the pipeline

--- web_crawler/tutorial/pipelines.py
+++ web_crawler/tutorial/pipelines.py
@@ -7,15 +7,27 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import json
+import re
 
 
 class TutorialPipeline:
+    double_quotes = '|'.join([chr(ord('\u201c')), chr(ord('\u201d'))])
+    single_quotes = '|'.join([chr(ord('\u2019')), chr(ord('\u2032'))])
+    dashes = '|'.join([chr(ord('\u2014'))])
+
     def open_spider(self, spider):
         self.results = []
 
     def process_item(self, item, spider):
-        self.results.append(ItemAdapter(item).asdict())
-        return item
+        _item = ItemAdapter(item).asdict()
+
+        # Remove the fancy characters
+        _item['text'] = re.sub(fr'{self.double_quotes}', '', _item['text'])
+        _item['text'] = re.sub(fr'{self.single_quotes}', "'", _item['text'])
+        _item['text'] = re.sub(fr'{self.dashes}', '---', _item['text'])
+
+        self.results.append(_item)
+        return _item
 
     def close_spider(self, spider):
         with open('results.json', 'w') as export_file:
